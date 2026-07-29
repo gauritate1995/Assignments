f65d0d191317e54c21a5847d3f996c531dd2303d
Gauri Tate <gauritate1995@gmail.com>
2026-07-29 08:47:29 +0530

    Assignment 1- Datatypes

---
+++ javascript-training/comments.js
@@ -0,0 +1,4 @@
+//console.log is to print the the statement 
+/* author: Gauri Tate
+Version: 1.0*/
+console.log("Addition of 2 numbers:", 2+2);
---
+++ javascript-training/hello-world.js
@@ -0,0 +1,4 @@
+console.log("Hello World");
+console.info("This is info message.");
+console.warn("This is warn message");
+console.error("This is error message");
